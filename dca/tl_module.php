d49e527ad7c801d554e850856e9415634d30d3bf
Mirco Kibiger <mkibiger@blioxxx.de>
2016-11-20 01:21:23 +0100

    add correct checkbox

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -1,8 +1,8 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_module']['palettes']['galiere'] = '{title_legend},name,type,bx-galiere-title;';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['galiere'] = '{title_legend},name,type,bxGaliereTitle;';
 
-$GLOBALS['TL_DCA']['tl_module']['fields']['bx-galiere-title'] = array
+$GLOBALS['TL_DCA']['tl_module']['fields']['bxGaliereTitle'] = array
 (
     'exclude'                 => true,
     'label'                   => &$GLOBALS['TL_LANG']['tl_module']['bx-galiere-title'],
