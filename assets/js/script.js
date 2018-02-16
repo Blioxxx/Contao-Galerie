de90ec3202cbf07aabbe40c46363048603dc55fc
Mirco Kibiger <mkibiger@blioxxx.de>
2018-02-17 00:50:26 +0100

    add new script to address the min-height
    (cherry picked from commit b50d95a)

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function setMinHeight() {
+    const images = document.getElementsByClassName('images');
+
+    var minHeight;
+
+    for (let item of images) {
+        if(item.clientHeight > minHeight) {
+            minHeight = item.clientHeight;
+        }
+    }
+
+    for (let item of images) {
+        item.style.minHeight = minHeight;
+    }
+
+}
